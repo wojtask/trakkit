53dd50a5a93eaf55812cfcf6f8d0c725b44289ef
Krzysztof Wojtas <kwojtas@gmail.com>
2018-03-17 23:19:27 +0100

    empty Spring Boot application

---
+++ src/main/java/pl/kwojtas/trakkit/TrakkitApplication.java
@@ -0,0 +1,12 @@
+package pl.kwojtas.trakkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrakkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrakkitApplication.class, args);
+	}
+}
